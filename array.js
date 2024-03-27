75a95ce4f37f0b1a6e9b70354fd5c136efc13f81
sawankumarmalviya <malviyasawan14@gmail.com>
2024-03-27 11:03:54 +0530

    .

--- array.js
+++ array.js
@@ -3,29 +3,265 @@
 
 //This method does not change the existing arrays, but instead returns a new array.
 
-function concat(arr1,arr2){
-arr1 = ["harda","timarni","seoni","hoshangabad"];
-arr2 = ["sodalpur","betul"];
+function concat(arr1, arr2) {
+  arr1 = ["harda", "timarni", "seoni", "hoshangabad"];
+  arr2 = ["sodalpur", "betul"];
 
-console.log(arr1.concat(arr2,arr1))// new array
-console.log(arr1)
-console.log(arr2)
+  console.log(arr1.concat(arr2, arr1))// new array
+  console.log(arr1)
+  console.log(arr2)
 
 }
 concat();
 
-function nestedconcat(arr1,arr2){
-  arr1 = [[1]]  ,
-  arr2 = [2,[3]]
-  arr3 = [['sawan','malviya']]
-  let result  = arr1.concat(arr2)
+function nestedconcat(arr1, arr2) {
+  arr1 = [[1]],
+    arr2 = [2, [3]]
+  arr3 = [['sawan', 'malviya']]
+  let result = arr1.concat(arr2)
   console.log(result)
   arr1[0].push(4)
   console.log(result)
-  console.log(arr1.concat(arr3)) 
+  console.log(arr1.concat(arr3))
 }
 nestedconcat();
-   
+
+function con() {
+  let arr1 = ['graps', 'apple', 'banana']
+  let arr2 = 'pinaple';
+  console.log(arr1.concat(arr2))
+}
+con();
+
+// Foreach function 
+
+//Logging each element of an array:
+
+let array = ["harda", "timarni", "seoni", "hoshangabad"];
+array.forEach(function (item, index) {
+  console.log(index, item)
+})
+
+let array2 = [1, 2, 3, 4]
+array2.forEach(function (item, index) {
+  console.log(index, item)
+})
+
+let array3 = [10, 20, 30, 40]
+array3.forEach(function (item, index) {
+  console.log(index, item);
+})
+
+let company = ['ibm', "microsoft", 'gallexy', 'facebook', 'google']
+company.forEach((item, index, arrr) => {
+  console.log(index, item)
+})
+
+// Modifying each element of an array:
+
+let number = [1, 2, 3]
+number.forEach((item, index, arr) => {
+  arr[index] = item * 2
+})
+console.log(number)
+
+//Summing up the values of an array:
+function summingArray() {
+  let numbers = [1, 2, 3, 4, 5, 6]
+  let sum = 0
+  numbers.forEach((item) => {
+    sum = sum + item
+  })
+  console.log(sum)
+
+  let multi = 1;
+  numbers.forEach((item, index, arr) => {
+    multi = multi * item
+  })
+  console.log(multi);
+
+}
+summingArray()
+
+//Removing duplicates from an array:
+const duplicates = [1, 2, 2, 3, 4, '4', 5];
+const unique = []
+duplicates.forEach((item) => {
+  if (!unique.includes(item)) {
+    unique.push(item)
+  }
+})
+console.log(duplicates)
+console.log(unique)
+
+const students = ['monu', 'rajendra', 'dinesh', 'dinesh', 'ranu', 'rajendra']
+const uniqueArray = [];
+students.forEach((student) => {
+  if (!uniqueArray.includes(student)) {
+    uniqueArray.push(student)
+  }
+})
+console.log(uniqueArray);
+
+const companys = ['honda', 'honda', 'mahindra', 'tata', 'hundai', 'RR', 'tata', 'mahindra']
+companys.forEach((car) => {
+  if (!uniqueArray.includes(car)) {
+    uniqueArray.push(car);
+  }
+})
+console.log(uniqueArray)
 
 
+//Filtering out elements based on a condition:
+const evenArray = [20, 1, 90, 50, 6, 8, 4, 5, 9, 2, 1, 3, 5]
+const even = []
+evenArray.forEach((item) => {
+  if (item % 3 == 0 && !even.includes(item)) {
+    even.push(item)
+  }
+})
 
+console.log(even)
+
+// pop method =======>
+//The pop() method removes the last element from an array:
+even.pop();
+console.log(even)
+even.pop()
+console.log(even)
+even.pop()
+console.log(even)
+console.log('pop')
+
+
+// push method =======>
+//The push() method adds a new element to an array (at the end):
+even.push(6)
+console.log(even)
+even.push(9)
+console.log(even)
+even.push(3)
+console.log(even);
+console.log("push")
+
+// join method ==========>
+//The join() method also joins all array elements into a string. It behaves just like toString(), but in addition you can specify the separator:
+const fru = ['banana', 'coconate', 'mango']
+console.log(fru.join(" # "))
+const nums = [10, 20, 30, 40, 50]
+console.log(nums.toString())
+console.log(fru)
+fru.shift()
+console.log(fru)
+fru.shift();
+console.log(fru)
+fru.unshift("coconate")
+console.log(fru)
+fru.unshift("banana")
+console.log(fru)
+
+console.log(fru)
+fru.fill('pinapple')
+console.log(fru)
+
+
+//filter method =====>
+// the filter() method is used to create a new array with all elements that pass a certain condition defined by a provided function.
+
+const Array = [10, 20, 15, 65, 35]
+let res = Array.filter((item) => {
+  if (item > 30) {
+    return item
+  }
+})
+console.log(res)
+// find prime number 
+const primeNumber = [1, 2, 3, 4, 5, 6, 20, 64, 58, 71]
+//  const isPrime = (item) =>{
+//       for(let i =2 ; i<item ; i++){
+//         if(item%i===0){
+//             return true
+//         }
+//       }
+//       return  item>1
+//  }
+//  const resPrime = primeNumber.filter(isPrime) 
+//  console.log(resPrime)
+console.log(primeNumber)
+const isPrime = primeNumber.filter((item) => {
+  for (let i = 2; i < item; i++) {
+    if (item % i === 0) {
+      return false;
+
+    }
+  }
+  return item > 1
+})
+console.log(isPrime)
+
+//even number using filter methode
+const num = [1, 2, 5, 8, 7, 9]
+const evenNum = num.filter((number) => {
+  if (number % 2 == 0) {
+    return number
+  }
+})
+console.log(evenNum)
+const ev = num.filter(number => number % 2 === 0)
+console.log(ev)
+
+
+//Filtering strings longer than 3 characters:
+const str = ['asdasdad', 'dfas', 'sad', 'asdasaf']
+const r = str.filter(string => string.length > 3)
+console.log(r);
+
+//Filtering objects based on a property value:
+
+const person = [
+  { names: 'Alina raise', age: 25  }, 
+  { names: 'srk', age: 52 },
+  { names: 'rishabh', age: 17}
+]
+console.log(person[0].names)
+console.log(person[1].names)
+console.log(person[2].names)
+let voter = person.filter((value) => {
+  if (value.age >= 18){
+   let name = value.names
+    return name
+  }
+})
+ console.log(voter)
+
+//Filtering out null or undefined values:
+ const values = [1, null, 2, undefined, 3];
+  const nonNullvalue = values.filter((item)=>{
+    if(item !== null && item !== undefined){
+      return item
+    }
+  })
+  console.log(nonNullvalue)
+  
+  let copy = []
+  let y = values.forEach((item)=>{
+    if(item!==null && item !==undefined){
+          copy.push(item)
+    }
+  } )
+  console.log(copy)
+  //Filtering unique elements from an array:
+  
+  
+  const uniqarray = [1,1,1,2,5,5,6,5,5,8,9,9,7]
+  const uniquearr = []
+  uniqarray.filter((item)=>{
+    if(!uniquearr.includes(item)){
+      uniquearr.push(item)
+    }
+  })
+ console.log(uniquearr)
+
+ //Filtering elements containing a specific substring:
+
+  
--- loops.js
+++ loops.js
@@ -1,44 +1,44 @@
 //Write a program to print your name 10 times.
-let names= 'sawan';
+let names = 'sawan';
 
 for (let i = 0; i < 10; i++) {
-    console.log(i+ ' ' + names)
+    console.log(i + ' ' + names)
 }
 
 //WAP to print your name n times.
-let n=5;
-for(let i=0; i<n;i++){
-    console.log(i+ ' ' +"sawan kumar n times")
+let n = 5;
+for (let i = 0; i < n; i++) {
+    console.log(i + ' ' + "sawan kumar n times")
 }
 
 //3. WAP to print given string 10 times
 let strings = 'this is the string'
-for(let i =0; i<10; i++){
+for (let i = 0; i < 10; i++) {
     console.log(strings)
 }
 //4. WAP to print given string n times.
-for(let i=0; i<n; i++){
+for (let i = 0; i < n; i++) {
     console.log(` ${i} ${strings}`)
 }
 //5. Write a program to print the first 10 natural numbers.
- for(let i=1; i<=10 ; i++){
-     console.log(i)
- }
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
 //6. WAP to print first n natural numbers.
-for(let i = 1; i<=n ; i++){
+for (let i = 1; i <= n; i++) {
     console.log(i)
 }
 
 //7. WAP to print counting from A to B
 console.log('**************')
 let a = 4;
-let b= 12;
-for(a; a<=b; a++){
+let b = 12;
+for (a; a <= b; a++) {
     console.log(a);
 }
 
 //8. WAP to print reverse counting from n to 1.
-for(let i=n; i>0; i--){
+for (let i = n; i > 0; i--) {
     console.log(i)
 }
 
@@ -49,72 +49,72 @@ for(let i=n; i>0; i--){
 
 // Write a program to print table of 2.
 let res;
-for(let i=1; i<=10;i++){
-    res= 2 + 'x' + i + '='+ i*2;
+for (let i = 1; i <= 10; i++) {
+    res = 2 + 'x' + i + '=' + i * 2;
     console.log(res)
 }
 
 //WAP to print table of n.
-let num= 10;
+let num = 10;
 let resn
-for(let i=1; i<=10 ;i++){
-    resn= `${num} x ${i} = ${num*i}`
+for (let i = 1; i <= 10; i++) {
+    resn = `${num} x ${i} = ${num * i}`
     console.log(resn)
 }
 
 //WAP to calculate the sum of first 10 natural number.
 let naturalNum = 10;
-let sum =0
-for(let i= 1; i<=naturalNum;i++){
- sum += i
+let sum = 0
+for (let i = 1; i <= naturalNum; i++) {
+    sum += i
 }
 console.log(sum)
 
 //WAP to calculate numA to the power of numB (using loop).
-  let numA = 5;
-  let numB = 3;
-  let numSum=1
-  for(let i =1; i<=numB; i++){
-    numSum*=numA
-  }
-  console.log(numSum)
+let numA = 5;
+let numB = 3;
+let numSum = 1
+for (let i = 1; i <= numB; i++) {
+    numSum *= numA
+}
+console.log(numSum)
 
-  //WAP to print all elements of an array.
-  let arr = [12,2,5,5,4,8]
-  for( let i=0; i<arr.length; i++){
+//WAP to print all elements of an array.
+let arr = [12, 2, 5, 5, 4, 8]
+for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
-  }
+}
 
-  //WAP to print sum of all elements of an array.
-  let arr2 = [1,2,3,4,100,'sawan']
-  let sumArray=0
-  for(let i = 0 ; i<arr2.length; i++){
-    sumArray +=arr2[i] 
-  }
+//WAP to print sum of all elements of an array.
+let arr2 = [1, 2, 3, 4, 100, 'sawan']
+let sumArray = 0
+for (let i = 0; i < arr2.length; i++) {
+    sumArray += arr2[i]
+}
 console.log(sumArray)
 
 // WAP to create number using array elements.
 let num2 = 0
-for(i=0; i<arr.length; i++ ){
-    num2=num2*10+arr[i]
+for (i = 0; i < arr.length; i++) {
+    num2 = num2 * 10 + arr[i]
 }
-console.log('num2=' +num2)
+console.log('num2=' + num2)
 
 //WAP to print digits of a given number.
 let dig = 245
 let strdig = dig.toString();
-for( let i = 0; i<strdig.length; i++){
+for (let i = 0; i < strdig.length; i++) {
     console.log(strdig[i])
-}  
+}
 
 
 //. WAP to print sum of digits of a given number.
 let givNUm = 245
 let strnum = givNUm.toString();
 let strnumadd = 0
-for( let i = 0; i<strnum.length; i++){
-    strnumadd+=parseInt( strnum[i])
-}  
+for (let i = 0; i < strnum.length; i++) {
+    strnumadd += parseInt(strnum[i])
+}
 console.log(strnumadd)
 
 //WAP to print reverse of given number (using loop).
@@ -123,19 +123,19 @@ console.log(strnumadd)
 let givennumber = 16052
 let rev = 0;
 
-while(givennumber >0){
-  let lasstdig = givennumber%10 
-  rev= rev*10+lasstdig;
-  givennumber = (givennumber-lasstdig)/10
-  
+while (givennumber > 0) {
+    let lasstdig = givennumber % 10
+    rev = rev * 10 + lasstdig;
+    givennumber = (givennumber - lasstdig) / 10
+
 }
 console.log(rev)
 
 let onenum = 1250;
 let revrs = 0
-while(onenum >0){
-    let last = onenum%10 //0
-    revrs = (revrs*10)+last //0*10+0= 0
+while (onenum > 0) {
+    let last = onenum % 10 //0
+    revrs = (revrs * 10) + last //0*10+0= 0
     onenum = Math.floor(onenum / 10);//(1250-0)/10=125
 }
 console.log(revrs);
@@ -143,37 +143,37 @@ console.log(revrs);
 
 // WAP to check a number is palindrom or not.
 
- let palindrom = 12521; // 161, 
+let palindrom = 12521; // 161, 
 let revers = 0;
 let numcopy = palindrom;
-while(palindrom!=0){
-    let xdig = palindrom%10
-    revers = revers*10+xdig;
-    palindrom = (palindrom-xdig)/10
+while (palindrom != 0) {
+    let xdig = palindrom % 10
+    revers = revers * 10 + xdig;
+    palindrom = (palindrom - xdig) / 10
 }
 
-if(revers==numcopy){
+if (revers == numcopy) {
     console.log('this number is palnidrom')
-}else{
+} else {
     console.log('this number is not a palindrom number')
 }
 
 //WAP to check a number is armstrong or not. (armstrong number is some of its digits each reside to a power) 
 
 let arm = 153; //
-let copyarm =arm;
+let copyarm = arm;
 let pwos = arm.toString().length;
-let intpow= 0
-while(arm!=0){
-    rem = arm%10
-    intpow += Math.pow(rem,pwos)
-    arm = (arm-rem)/10
+let intpow = 0
+while (arm != 0) {
+    rem = arm % 10
+    intpow += Math.pow(rem, pwos)
+    arm = (arm - rem) / 10
 }
 console.log(intpow)
-if(intpow==copyarm){
+if (intpow == copyarm) {
     console.log(`${intpow} is armstrong num`)
-}else{
-    console.log(`${intpow} is not armstrong num`)   
+} else {
+    console.log(`${intpow} is not armstrong num`)
 }
 
 //WAP to check a number is perfect number or not. 
@@ -182,38 +182,66 @@ let perfectNUm = 12;
 
 let sumperfectNUm = 0;
 
-for(let i = 0; i<=perfectNUm/2; i++){
-    if(perfectNUm%i==0){
-        sumperfectNUm= sumperfectNUm+i 
+for (let i = 0; i <= perfectNUm / 2; i++) {
+    if (perfectNUm % i == 0) {
+        sumperfectNUm = sumperfectNUm + i
     }
 }
-if(perfectNUm== sumperfectNUm){
+if (perfectNUm == sumperfectNUm) {
     console.log(`${perfectNUm} is perfect number`)
-}else{
-    console.log(`${perfectNUm} is not perfect number`)  
+} else {
+    console.log(`${perfectNUm} is not perfect number`)
 }
 
-//WAP to check a number is prime or not.
-let primeNUmber = 5;
-for(let i =2; i<=primeNUmber; i++){
-
-}
 
 // nested loop 
 
-for(let i = 1; i<=10; i++){// 1,2,3,4,5,6,7,8,9,10
-    let ba= ""
-    for(let k = 1; k<=10 ; k++){// 1,2,3,4,5,6,7,8,9,10
-     ba = ba +   (i*k) +'\t'
-} 
-console.log(ba)
-} 
+for (let i = 1; i <= 10; i++) {// 1,2,3,4,5,6,7,8,9,10
+    let ba = ""
+    for (let k = 1; k <= 10; k++) {// 1,2,3,4,5,6,7,8,9,10
+        ba = ba + (i * k) + '\t'
+    }
+    console.log(ba)
+}
 // print  a tringle pattern
 
-for(let i = 1 ; i<10; i++){
+for (let i = 1; i < 10; i++) {
     let fa = ""
-    for(let j=1; j<i+1 ;j++){
-      fa = fa + i 
+    for (let j = 1; j < i + 1; j++) {
+        fa = fa + i
     }
     console.log(fa)
 }
+
+//WAP to check a number is prime or not. ya to 1 se ya khud se divide hona chie 
+// first loop 2 to 10
+//
+const primeNUm = 10;
+for (let i = 2; i < primeNUm; i++) {
+    let isPrimeNUm = true
+    for (let j = 2; j < i; j++) {
+        
+        if (i % j === 0) {
+            isPrimeNUm = false;
+            break;
+        }
+    }
+    // if (isPrimeNUm) {
+    //     console.log(i , "is prime number ")
+    // }
+}
+
+
+for(let i = 2; i<10; i++){
+    if(i%2===0){
+        console.log(i,"even");
+    }else{
+        console.log(i,"odd")
+    }
+}
+
+
+
+
+
+
